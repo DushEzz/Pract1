81ece5a7b8fc1a65ad787e88c6ffd7baebbe0cbf
DushEzz <footbar05@gmail.com>
2020-10-15 19:06:18 +0300

    Реализоваы все функции из homework01

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -15,7 +15,14 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     ''
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    for i in plaintext:
+        if i.isalpha() == True:
+            if (i == "z") or (i == "Z") or (i == "y") or (i == "Y") or (i == "x") or (i == "X"):
+                ciphertext += chr(ord(i) - 23)
+            else:
+                ciphertext += chr(ord(i) + shift)
+        else:
+            ciphertext += i
     return ciphertext
 
 
@@ -33,7 +40,14 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     ''
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    for i in ciphertext:
+        if i.isalpha() == True:
+            if (i == "a") or (i == "A") or (i == "b") or (i == "B") or (i == "c") or (i == "C"):
+                plaintext += chr(ord(i) + 23)
+            else:
+                plaintext += chr(ord(i) - shift)
+        else:
+            plaintext += i
     return plaintext
 
 
@@ -42,5 +56,19 @@ def caesar_breaker_brute_force(ciphertext: str, dictionary: tp.Set[str]) -> int:
     Brute force breaking a Caesar cipher.
     """
     best_shift = 0
-    # PUT YOUR CODE HERE
+    for i in dictionary:
+        if len(i) == len(ciphertext):
+            if (ord(ciphertext[0]) < ord(i[0])):
+                k = ord(ciphertext[0]) + 26 - ord(i[0])
+            else:
+                k = ord(ciphertext[0]) - ord(i[0])
+            for j in range(len(i)):
+                if (ord(ciphertext[0]) < ord(i[0])):
+                    k1 = ord(ciphertext[j]) + 26 - ord(i[j])
+                else:
+                    k1 = ord(ciphertext[j]) - ord(i[j])
+                if k != k1:
+                    break
+                else:
+                    best_shift = k
     return best_shift
