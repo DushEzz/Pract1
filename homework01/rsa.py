041ffc99ca760323438e6545342dbd6924b79c13
DushEzz <footbar05@gmail.com>
2020-10-16 21:57:30 +0300

    Реализованы функции из файла rsa.py

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -13,7 +13,11 @@ def is_prime(n: int) -> bool:
     >>> is_prime(8)
     False
     """
-    # PUT YOUR CODE HERE
+    p = True
+    for i in range(2, n // 2):
+        if n % i == 0:
+            p = False
+    return p
     pass
 
 
@@ -26,7 +30,12 @@ def gcd(a: int, b: int) -> int:
     >>> gcd(3, 7)
     1
     """
-    # PUT YOUR CODE HERE
+    while a != b:
+        if a > b:
+            a = a - b
+        else:
+            b = b - a
+    return a
     pass
 
 
@@ -38,7 +47,10 @@ def multiplicative_inverse(e: int, phi: int) -> int:
     >>> multiplicative_inverse(7, 40)
     23
     """
-    # PUT YOUR CODE HERE
+    for i in range(1, phi):
+        if i * e % phi == 1:
+            return i
+            break
     pass
 
 
@@ -49,10 +61,10 @@ def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[in
         raise ValueError("p and q cannot be equal")
 
     # n = pq
-    # PUT YOUR CODE HERE
+    n = p * q
 
     # phi = (p-1)(q-1)
-    # PUT YOUR CODE HERE
+    phi = (p - 1) * (q - 1)
 
     # Choose an integer e such that e and phi(n) are coprime
     e = random.randrange(1, phi)
