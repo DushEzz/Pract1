e5be26c330dd716b47f63431ceeabea7be323d67
DushEzz <footbar05@gmail.com>
2020-10-16 20:17:55 +0300

    Реализованы функции из файла vigenere.py

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -10,7 +10,20 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    if len(plaintext) > len(keyword):
+        keyword *= (len(plaintext) // len(keyword)) + 1
+    kwcp = keyword.upper()
+    for i in range(len(plaintext)):
+        j = ord(plaintext[i]) + ord(kwcp[i]) - 65
+        s = ord(plaintext[i])
+        if (j > 90) and (s < 91):
+            ciphertext += chr(ord(plaintext[i]) + ord(kwcp[i]) - 65 - 26)
+        elif (j <= 90) and (s < 91):
+            ciphertext += chr(ord(plaintext[i]) + ord(kwcp[i]) - 65)
+        elif (s > 96) and (j > 123):
+            ciphertext += chr(ord(plaintext[i]) + ord(kwcp[i]) - 65 - 26)
+        else:
+            ciphertext += chr(ord(plaintext[i]) + ord(kwcp[i]) - 65)
     return ciphertext
 
 
@@ -26,5 +39,20 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    if len(ciphertext) > len(keyword):
+        keyword *= (len(ciphertext) // len(keyword)) + 1
+    kwcp = keyword.upper()
+    for i in range(len(ciphertext)):
+        j = ord(ciphertext[i]) - ord(kwcp[i]) + 65
+        s = ord(ciphertext[i])
+        if (j < 65) and (s < 91):
+            plaintext += chr(ord(ciphertext[i]) - ord(kwcp[i]) + 65 + 26)
+        elif (j >= 65) and (s < 91):
+            plaintext += chr(ord(ciphertext[i]) - ord(kwcp[i]) + 65)
+        elif (s > 96) and (j < 96):
+            plaintext += chr(ord(ciphertext[i]) - ord(kwcp[i]) + 65 + 26)
+        else:
+            plaintext += chr(ord(ciphertext[i]) - ord(kwcp[i]) + 65)
     return plaintext
+
+print(encrypt_vigenere("PYTHON", "A"))
