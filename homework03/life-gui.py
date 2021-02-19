9f1040e901a60480f33a9906215c604c712d10f4
DushEzz <footbar05@gmail.com>
2021-02-19 15:44:46 +0300

    Вторая версия игры (все тесты пройдены)

--- homework03/life-gui.py
+++ homework03/life-gui.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+from pathlib import Path
+
 import pygame
 from pygame.locals import *
 
@@ -6,68 +9,93 @@ from ui import UI
 
 
 class GUI(UI):
-
     def __init__(self, life: GameOfLife, cell_size: int = 10, speed: int = 10) -> None:
         super().__init__(life)
+        # Размер клетки
         self.cell_size = cell_size
-        self.speed = speed
-        self.screen_size = self.cell_size * self.life.cols, self.cell_size * self.life.rows
+
+        # Ширина и высота окна
+        self.width = self.life.cols * self.cell_size
+        self.height = self.life.rows * self.cell_size
+        self.screen_size = self.width, self.height
+
+        # Инициализация окна
         self.screen = pygame.display.set_mode(self.screen_size)
 
+        # Скорость игры
+        self.speed = speed
+
+        # Цвета клеток
+        self.primary_color = arguments.primary_color
+        self.secondary_color = arguments.secondary_color
+
     def draw_lines(self) -> None:
-        for x in range(0, self.cell_size * self.life.cols, self.cell_size):
-            pygame.draw.line(self.screen, pygame.Color('black'),
-                             (x, 0), (x, self.cell_size * self.life.rows))
-        for y in range(0, self.cell_size * self.life.rows, self.cell_size):
-            pygame.draw.line(self.screen, pygame.Color('black'),
-                             (0, y), (self.cell_size * self.life.cols, y))
-        pass
+        """ Отрисовать сетку """
+        for x in range(0, self.width, self.cell_size):
+            pygame.draw.line(self.screen, pygame.Color("black"), (x, 0), (x, self.height))
+        for y in range(0, self.height, self.cell_size):
+            pygame.draw.line(self.screen, pygame.Color("black"), (0, y), (self.width, y))
 
     def draw_grid(self) -> None:
-        s1 = 0
-        s2 = 0
-        for i in self.life.curr_generation:
-            for j in i:
-                if j == 0:
-                    pygame.draw.rect(self.screen, pygame.Color('white'),
-                                     (self.cell_size * s1, self.cell_size * s2, self.cell_size, self.cell_size))
-                else:
-                    pygame.draw.rect(self.screen, pygame.Color('green'),
-                                     (self.cell_size * s1, self.cell_size * s2, self.cell_size, self.cell_size))
-                s1 += 1
-            s1 = 0
-            s2 += 1
-        pass
+        for pos_x, row in enumerate(self.life.curr_generation):
+            for pos_y, col in enumerate(row):
+                color = self.secondary_color
+                if col:
+                    color = self.primary_color
+                pygame.draw.rect(
+                    self.screen,
+                    color,
+                    (
+                        self.cell_size * pos_y + 1,
+                        self.cell_size * pos_x + 1,
+                        self.cell_size - 1,
+                        self.cell_size - 1,
+                    ),
+                )
 
     def run(self) -> None:
-        pygame.init()
+        """
+        Запустить игру.
+
+        Управление:
+        Пробел - Пауза.
+        Esc - Выход из игры.
+        Нажатие ЛКМ - "закрасить" клетку.
+        Вверх - сохранение "жизни" в файл вида life_*time*.txt
+        """
         clock = pygame.time.Clock()
-        pygame.display.set_caption('Game of Life')
-        self.screen.fill(pygame.Color('white'))
-        is_paused = True
-        while True:
+        pygame.display.set_caption("Game of Life")
+        self.screen.fill(pygame.Color("white"))
+        running = True
+        paused = False
+        while running:
+            if not self.life.is_changing or self.life.is_max_generations_exceeded:
+                running = False
             for event in pygame.event.get():
-                if event.type == QUIT:
-                    pygame.quit()
-                elif event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_SPACE:
-                        is_paused = not is_paused
-                elif event.type == pygame.MOUSEBUTTONDOWN:
-                    if event.button == 1:
-                        if is_paused:
-                            if self.life.curr_generation[event.pos[1] // self.cell_size][event.pos[0] // self.cell_size] == 0:
-                                self.life.curr_generation[event.pos[1] // self.cell_size][event.pos[0] // self.cell_size] = 1
-                            else:
-                                self.life.curr_generation[event.pos[1] // self.cell_size][event.pos[0] // self.cell_size] = 0
-                            if self.life.generations != self.life.max_generations:
-                                self.draw_grid()
-                                self.draw_lines()
-                                pygame.display.flip()
-            if not is_paused:
-                if not self.life.is_max_generations_exceeded and self.life.is_changing:
-                    self.life.step()
-                    self.draw_grid()
-                    self.draw_lines()
-                    pygame.display.flip()
-                    clock.tick(self.speed)
-        pass
+                if event.type == QUIT:  # type: ignore
+                    running = False
+                if event.type == KEYDOWN:  # type: ignore
+                    if event.key == K_ESCAPE:  # type: ignore
+                        running = False
+                    elif event.key == K_SPACE:  # type: ignore
+                        paused = True if not paused else False
+                    elif event.key == K_DOWN:  # type: ignore
+                        save_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                        self.life.save(Path(f"saves/life_{save_time}.txt"))
+                if event.type == MOUSEBUTTONDOWN:  # type: ignore
+                    click_position = pygame.mouse.get_pos()
+                    pos_x, pos_y = (
+                        click_position[1] // self.cell_size,
+                        click_position[0] // self.cell_size,
+                    )
+                    if self.life.curr_generation[pos_x][pos_y]:
+                        self.life.curr_generation[pos_x][pos_y] = 0
+                    else:
+                        self.life.curr_generation[pos_x][pos_y] = 1
+            self.draw_lines()
+            self.draw_grid()
+            if not paused:
+                self.life.step()
+            pygame.display.flip()
+            clock.tick(self.speed)
+        pygame.quit()
--- homework03/life.py
+++ homework03/life.py
@@ -1,20 +1,22 @@
 import pathlib
 import random
+import typing as tp
+from copy import deepcopy
 
-from typing import List, Optional, Tuple
+import pygame
+from pygame.locals import *
 
-Cell = Tuple[int, int]
-Cells = List[int]
-Grid = List[Cells]
+Cell = tp.Tuple[int, int]
+Cells = tp.List[int]
+Grid = tp.List[Cells]
 
 
 class GameOfLife:
-
     def __init__(
-            self,
-            size: Tuple[int, int],
-            randomize: bool = True,
-            max_generations: Optional[float] = float('inf')
+        self,
+        size: tp.Tuple[int, int],
+        randomize: bool = True,
+        max_generations: tp.Optional[float] = float("inf"),
     ) -> None:
         # Размер клеточного поля
         self.rows, self.cols = size
@@ -22,137 +24,129 @@ class GameOfLife:
         self.prev_generation = self.create_grid()
         # Текущее поколение клеток
         self.curr_generation = self.create_grid(randomize=randomize)
-        # Резервный список клеток
-        self.pp = self.prev_generation.copy()
         # Максимальное число поколений
         self.max_generations = max_generations
         # Текущее число поколений
         self.generations = 1
 
     def create_grid(self, randomize: bool = False) -> Grid:
-        life_list = [[0] * self.cols for i in range(self.rows)]
+        """
+        Создание списка клеток.
+        Клетка считается живой, если ее значение равно 1, в противном случае клетка
+        считается мертвой, то есть, ее значение равно 0.
+        Parameters
+        ----------
+        randomize : bool
+            Если значение истина, то создается матрица, где каждая клетка может
+            быть равновероятно живой или мертвой, иначе все клетки создаются мертвыми.
+        Returns
+        ----------
+        out : Grid
+            Матрица клеток размером `rows` х `cols`.
+        """
         if randomize:
-            life_list = []
-            for i in range(self.rows):
-                part = []
-                for j in range(self.cols):
-                    part.append(random.randint(0, 1))
-                life_list.append(part)
-        return life_list
-        pass
+            return [[random.randint(0, 1) for _ in range(self.cols)] for _ in range(self.rows)]
+        return [[0 for _ in range(self.cols)] for _ in range(self.rows)]
 
     def get_neighbours(self, cell: Cell) -> Cells:
-        if cell == (0, 0):
-            return [self.curr_generation[0][1], self.curr_generation[1][1], self.curr_generation[1][0]]
-        elif cell == (0, self.cols - 1):
-            return [self.curr_generation[0][cell[1] - 1], self.curr_generation[1][cell[1] - 1], self.curr_generation[1][cell[1]]]
-        elif cell == (self.rows - 1, self.cols - 1):
-            return [self.curr_generation[cell[0] - 1][cell[1] - 1], self.curr_generation[cell[0] - 1][cell[1]], self.curr_generation[cell[0]][cell[1] - 1]]
-        elif cell == (self.rows - 1, 0):
-            return [self.curr_generation[cell[0] - 1][0], self.curr_generation[cell[0] - 1][1], self.curr_generation[self.curr_generation[cell[0]][1]]]
-        elif cell[0] == 0:
-            return [self.curr_generation[0][cell[1] + 1], self.curr_generation[0][cell[1] - 1], self.curr_generation[1][cell[1] - 1], self.curr_generation[1][cell[1]],
-                    self.curr_generation[1][cell[1] + 1]]
-        elif cell[1] == 0:
-            return [self.curr_generation[cell[0] + 1][0], self.curr_generation[cell[0] - 1][0], self.curr_generation[cell[0] + 1][1], self.curr_generation[cell[0] - 1][1],
-                    self.curr_generation[cell[0]][1]]
-        elif cell[0] == self.rows - 1:
-            return [self.curr_generation[cell[0]][cell[1] - 1], self.curr_generation[cell[0]][cell[1] + 1], self.curr_generation[cell[0] - 1][cell[1] - 1],
-                    self.curr_generation[cell[0] - 1][cell[1]], self.curr_generation[cell[0] - 1][cell[1] + 1]]
-        elif cell[1] == self.cols - 1:
-            return [self.curr_generation[cell[0] + 1][cell[1]], self.curr_generation[cell[0] - 1][cell[1]], self.curr_generation[cell[0] + 1][cell[1] - 1],
-                    self.curr_generation[cell[0] - 1][cell[1] - 1], self.curr_generation[cell[0]][cell[1] - 1]]
-        else:
-            return [self.curr_generation[cell[0] + 1][cell[1] - 1], self.curr_generation[cell[0] + 1][cell[1]], self.curr_generation[cell[0] + 1][cell[1] + 1],
-                    self.curr_generation[cell[0]][cell[1] - 1], self.curr_generation[cell[0]][cell[1] + 1], self.curr_generation[cell[0] - 1][cell[1] - 1],
-                    self.curr_generation[cell[0] - 1][cell[1]], self.curr_generation[cell[0] - 1][cell[1] + 1]]
-        pass
+        """
+        Вернуть список соседних клеток для клетки `cell`.
+        Соседними считаются клетки по горизонтали, вертикали и диагоналям,
+        то есть, во всех направлениях.
+        Parameters
+        ----------
+        cell : Cell
+            Клетка, для которой необходимо получить список соседей. Клетка
+            представлена кортежем, содержащим ее координаты на игровом поле.
+        Returns
+        ----------
+        out : Cells
+            Список соседних клеток.
+        """
+        neighbours = []
+        rows = len(self.curr_generation)
+        cols = len(self.curr_generation[1]) if rows else 0
+        for row in range(max(0, cell[0] - 1), min(rows, cell[0] + 2)):
+            for col in range(max(0, cell[1] - 1), min(cols, cell[1] + 2)):
+                if (row, col) != cell:
+                    neighbours.append(self.curr_generation[row][col])
+        return neighbours
 
     def get_next_generation(self) -> Grid:
-        dict_neib = dict()
-        next_gen = self.curr_generation
-        for i in range(self.rows):
-            for j in range(self.cols):
-                dict_neib[(i, j)] = self.get_neighbours((i, j))
-        for i in dict_neib.keys():
-            if next_gen[i[0]][i[1]] == 0:
-                if dict_neib[i].count(1) == 3:
-                    next_gen[i[0]][i[1]] = 1
-            else:
-                if dict_neib[i].count(1) != 3 and dict_neib[i].count(1) != 2:
-                    next_gen[i[0]][i[1]] = 0
-        return next_gen
-        pass
+        """
+        Получить следующее поколение клеток.
+        Returns
+        ----------
+        out : Grid
+            Новое поколение клеток.
+        """
+        new_grid = deepcopy(self.curr_generation)
+        for pos_x, row in enumerate(self.curr_generation):
+            for pos_y, col in enumerate(row):
+                pos = (pos_x, pos_y)
+                neigh = sum(self.get_neighbours(pos))
+                if col:
+                    if neigh != 2 and neigh != 3:
+                        new_grid[pos_x][pos_y] = 0
+                    else:
+                        new_grid[pos_x][pos_y] = 1
+                else:
+                    if neigh == 3:
+                        new_grid[pos_x][pos_y] = 1
+        return new_grid
 
     def step(self) -> None:
         """
         Выполнить один шаг игры.
         """
-        self.pp = self.prev_generation.copy()
-        self.prev_generation.clear()
-        part = []
-        for i in range(self.rows):
-            for j in range(self.cols):
-                part.append(self.curr_generation[i][j])
-            self.prev_generation.append(part)
-            part = []
-        self.curr_generation = self.get_next_generation()
-        self.generations += 1
-        pass
+        if not self.is_max_generations_exceeded:
+            self.prev_generation, self.curr_generation = (
+                self.curr_generation,
+                self.get_next_generation(),
+            )
+            self.generations += 1
 
     @property
     def is_max_generations_exceeded(self) -> bool:
         """
         Не превысило ли текущее число поколений максимально допустимое.
         """
-        if self.generations >= self.max_generations:
+        if self.generations >= self.max_generations:  # type: ignore
             return True
-        else:
-            return False
-        pass
+        return False
 
     @property
     def is_changing(self) -> bool:
         """
         Изменилось ли состояние клеток с предыдущего шага.
         """
-        if self.curr_generation == self.prev_generation:
-            return False
-        else:
+        if self.prev_generation != self.curr_generation:
             return True
-        pass
+        return False
 
     @staticmethod
-    def from_file(filename: pathlib.Path) -> 'GameOfLife':
+    def from_file(filename: pathlib.Path) -> "GameOfLife":
         """
-        Прочитать состояние клеток из указанного файла.
+        Прочитать состояние клеток из указанного файла  .
         """
-        file_life = open(filename, 'r')
-        e = []
-        s = []
-        for i in file_life.read():
-            if i != '\n':
-                e.append(int(i))
-            else:
-                s.append(e)
-                e = []
-        s.append(e)
-        new_class = GameOfLife((len(s), len(s[0])))
-        new_class.curr_generation = s
-        file_life.close()
-        return new_class
-        pass
+        new_grid = []
+        with open(filename) as file:
+            for row in file:
+                new_grid.append([int(val) for val in row if (val == "0" or val == "1")])
+        new_game = GameOfLife((len(new_grid), len(new_grid[0])))
+        new_game.curr_generation = new_grid
+        return new_game
 
     def save(self, filename: pathlib.Path) -> None:
         """
         Сохранить текущее состояние клеток в указанный файл.
         """
-        file_life = open(filename, 'a')
-        s = self.curr_generation
-        for i in range(len(s)):
-            for j in range(len(s[0])):
-                file_life.write(str(s[i][j]))
-            if i != len(s) - 1:
-                file_life.write('\n')
-        file_life.close()
-        pass
+        if not pathlib.Path("saves").exists():
+            pathlib.Path("saves").mkdir()
+        if not pathlib.Path(filename).exists():
+            pathlib.Path(filename).touch()
+        with open(filename, "w") as file:
+            for row in range(self.rows):
+                for col in range(self.cols):
+                    file.write(str(self.curr_generation[row][col]))
+                file.write("\n")
